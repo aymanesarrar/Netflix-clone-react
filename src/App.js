5359c896c0b55410c49b424e097fd7dc10aa6c47
Aymane sarrar <sarrarayman@yahoo.fr>
2020-09-02 00:38:43 +0100

    setting up axios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+import Row from './Components/Row';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Salam React</h1>
-    </div>
-  );
+		<div className="App">
+			<h1>Salam React</h1>
+			<Row title="NETFLIX ORIGINALS" />
+			<Row title="Trending now" />
+		</div>
+	);
 }
 
 export default App;
