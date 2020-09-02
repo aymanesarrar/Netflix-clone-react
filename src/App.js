7e8b18cd5e31a88faa519ee428117d9444e03998
Aymane sarrar <sarrarayman@yahoo.fr>
2020-09-02 17:12:22 +0100

    styling the rows and the banner + fetching movies

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import './App.css';
 import Row from './Components/Row';
+import requests from './requests'
+import Banner from './Components/Banner'
 
 
 function App() {
   return (
-		<div className="App">
-			<h1>Salam React</h1>
-			<Row title="NETFLIX ORIGINALS" />
-			<Row title="Trending now" />
+		<div className="app">
+			{/* Navbar */}
+			<Banner />
+			<Row isLargeRow title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
+			<Row title="Trending now" fetchUrl={requests.fetchTrending}/>
+			<Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+			<Row title="Action Movies" fetchUrl={requests.fetchActionMovie}/>
+			<Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovie}/>
+			<Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovie}/>
+			<Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovie}/>
+			<Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
 		</div>
 	);
 }
