dfd87543ba4349f1fd37ee9f6d6e460842050e5d
Aymane sarrar <sarrarayman@yahoo.fr>
2020-09-03 01:30:03 +0100

    some additional part for navbar

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -17,7 +17,8 @@ function Nav() {
   return (
     <div className={`nav ${show && "nav__black"}`}>
       <img src="https://www.clipartmax.com/png/full/215-2154144_transparent-background-netflix-logo.png" alt="Netflix logo" className="nav__logo"/>
-      <img src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/366be133850498.56ba69ac36858.png" alt="avatar logo" className="nav__avatar"/>
+      <a href="https://github.com/aymanesarrar"><img src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/366be133850498.56ba69ac36858.png" alt="avatar logo" className="nav__avatar" /></a>
+
     </div>
   )
 }
